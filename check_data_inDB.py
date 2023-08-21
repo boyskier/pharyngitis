595af62b524865a6cd5b62dab717f5a9b9a9e818
boyskier_gram <boy.skier@gmail.com>
2023-08-22 01:40:20 +0900

    image plot 부분 subplot으로 수정

--- check_data_inDB.py
+++ check_data_inDB.py
@@ -60,6 +60,27 @@ def show_image_data_from_image_id(image_id, table_name):
         print(probability)
 
 
+# def show_all_images_by_user_name(user_name, table_name):
+#     connection = connect_db()
+#     cursor = connection.cursor()
+#     query = f"SELECT image_data, upload_time, probability FROM {table_name} WHERE user_name = '{user_name}';"
+#     cursor.execute(query)
+#     results = cursor.fetchall()
+#     close_db(connection, cursor)
+#
+#     if not results:
+#         print(f"No records found for user {user_name}")
+#         return None
+#
+#     total_images = len(results)
+#     print(f"Total {table_name} images for user {user_name}: {total_images}")
+#
+#     for image_data, upload_time, probability in results:
+#         title = f"{user_name}_{upload_time.strftime('%Y%m%d_%H%M%S')}_{probability:.3f}"
+#         show_image(image_data, title)
+#         print(f"Upload Time: {upload_time}")
+#         print(f"Probability: {probability}")
+
 def show_all_images_by_user_name(user_name, table_name):
     connection = connect_db()
     cursor = connection.cursor()
@@ -73,13 +94,29 @@ def show_all_images_by_user_name(user_name, table_name):
         return None
 
     total_images = len(results)
+    rows = 2
+    cols = (total_images + rows - 1) // rows
     print(f"Total {table_name} images for user {user_name}: {total_images}")
 
-    for image_data, upload_time, probability in results:
+    fig, axes = plt.subplots(rows, cols, figsize=(20, 10))  # 2행과 열의 수를 설정합니다.
+    fig.subplots_adjust(wspace=0.5, hspace=0.5)  # 서브플롯 간 간격을 늘립니다.
+
+    if rows == 1 or cols == 1:
+        axes = axes.reshape(rows, cols)  # 1차원 배열인 경우, 형상을 바꿉니다.
+
+    for i, (image_data, upload_time, probability) in enumerate(results):
         title = f"{user_name}_{upload_time.strftime('%Y%m%d_%H%M%S')}_{probability:.3f}"
-        show_image(image_data, title)
-        print(f"Upload Time: {upload_time}")
-        print(f"Probability: {probability}")
+        image_stream = io.BytesIO(image_data)
+        image = Image.open(image_stream)
+
+        ax = axes[i // cols, i % cols]
+        ax.imshow(image)
+        ax.set_title(title, fontsize=10)
+        ax.axis('off')
+
+    plt.show()
+
+
 
 
 user_name = 'patient2'  # 원하는 환자 이름으로 수정
